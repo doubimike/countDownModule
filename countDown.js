91a117a0832cf5f032160ee84a83ae054cd5db45
mike.zhang <mike.zhang@yeamoney.cn>
2016-06-13 11:54:13 +0800

    用构造函数的方法重写，解决一个页面只能绑定一个计时器的bug

--- countDown.js
+++ countDown.js
@@ -1,26 +1,35 @@
 /**
  * [countDown description]
- * $对象的flag属性即是flag，就不用污染全局对象了
+ * $对象的flag属性即是flag，就不用污染全局对象了，现在只支持onclick，不支持addeventlistener。
  * @param  {[obj]} el       [需要绑定的dom元素]
  * @param  {[num]} leftTime [总共需要倒计的时间]
  * @return {[type]}          [无]
  */
-var $ = {
-    flag: 1,
-    countDown: function countDown(el, leftTime) {
-        if ($.flag) {
-            $.flag = 0;
+
+function CountDown() {
+    this.flag = 1;
+}
+
+CountDown.prototype.countDown = function(el, leftTime) {
+    var _left = leftTime;
+    if (this.flag) {
+        this.flag = 0;
+        el.onclick = function() {
+            return false;
+        };
+        el.innerHTML = '等待' + leftTime + 'S';
+        var countDownIntervalId = setInterval(function() {
+            leftTime = leftTime - 1;
             el.innerHTML = '等待' + leftTime + 'S';
-            var countDownIntervalId = setInterval(function() {
-                leftTime = leftTime - 1;
-                el.innerHTML = '等待' + leftTime + 'S';
-                if (leftTime == 0) {
-                    clearInterval(countDownIntervalId);
-                    el.innerHTML = '获取验证码';
-                    left = 60;
-                    $.flag = 1;
-                }
-            }, 1000);
-        }
+            if (leftTime == 0) {
+                clearInterval(countDownIntervalId);
+                el.innerHTML = '获取验证码';
+                leftTime = _left;
+                this.flag = 1;
+                el.onclick = function() {
+                    new CountDown().countDown(el, leftTime);
+                };
+            }
+        }, 1000);
     }
 };
